2ae6c25e294328247318c52ef8377cb2df6c2c6d
ptlm500 <peter_mills@hotmail.co.uk>
2020-02-12 19:13:30 +0000

    Initial babel and webpack setup, adding App.js

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # todo-app
+
+## Installation instructions
+
+1. Clone the repository
+2. Run `npm install`
+3. Run `npm start`
---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './app.scss';
+
+export default function App() {
+  return (
+    <div className='app'>
+      <h1>Welcome to TODO app</h1>
+    </div>
+  );
+}
