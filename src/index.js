a993bab9dc4456bc8c10865520c995699e560b6d
ptlm500 <peter_mills@hotmail.co.uk>
2020-02-12 20:41:24 +0000

    Add store, react-redux provider

--- src/index.js
+++ src/index.js
@@ -1,4 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-ReactDOM.render(<App />, document.getElementById('root'));
+import { Provider } from 'react-redux';
+import store from './state/store';
+
+const rootElement = document.getElementById('root');
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  rootElement
+);
