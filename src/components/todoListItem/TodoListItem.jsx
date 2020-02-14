5269d0f276c4798db91689c72759dd9ba64ce6c4
ptlm500 <peter_mills@hotmail.co.uk>
2020-02-14 21:31:52 +0000

    Use InlineEditable component

--- src/components/todoListItem/TodoListItem.jsx
+++ src/components/todoListItem/TodoListItem.jsx
@@ -1,30 +1,71 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { removeTodo } from '../../state/actions/actions';
+import { removeTodo, updateTodo } from '../../state/actions/actions';
+import InlineEditable from '../inline-editable';
+import TextareaAutosize from 'react-autosize-textarea';
+
+import './todoListItem.scss';
 
 export function TodoListItem(props) {
   const {
     id,
-    name,
-    description,
     creationDate,
-    onRemove
+    onRemove,
+    onUpdate
   } = props;
+  const nameInStore = props.name;
+  const descriptionInStore = props.description;
+
+  const [description, setDescription] = useState(descriptionInStore);
+  const [name, setName] = useState(nameInStore);
+
+  useEffect(() => {
+    // Set description state if stored description changes
+    setDescription(descriptionInStore);
+  }, [descriptionInStore]);
+
+  useEffect(() => {
+    // Set name state if stored name changes
+    setName(nameInStore);
+  }, [nameInStore]);
 
   function remove() {
     onRemove(id);
   }
 
+  function updateName(newName) {
+    newName !== nameInStore && onUpdate({id, name: newName});
+  }
+
+  function updateDescription(newDescription) {
+    newDescription !== descriptionInStore &&
+      onUpdate({id, description: newDescription});
+  }
+
   return (
     <li className='todo-list__item'>
-      <div>
-        <h3>{name}</h3>
+      <section>
+        <InlineEditable
+          value={name}
+          onSubmit={updateName}
+        >
+          <input
+            onChange={e => setName(e.target.value)}
+          />
+        </InlineEditable>
         <button onClick={remove}>
           Remove
         </button>
-      </div>
-      <a>{description}</a>
+      </section>
+      <InlineEditable
+        value={description}
+        onSubmit={updateDescription}
+      >
+        <TextareaAutosize
+          onChange={e => setDescription(e.target.value)}
+        />
+      </InlineEditable>
       <a>{creationDate}</a>
     </li>
   );
@@ -35,12 +76,14 @@ TodoListItem.propTypes = {
   name: PropTypes.string.isRequired,
   description: PropTypes.string.isRequired,
   creationDate: PropTypes.string.isRequired,
-  onRemove: PropTypes.func.isRequired
+  onRemove: PropTypes.func.isRequired,
+  onUpdate: PropTypes.func.isRequired
 };
 
 const mapDispatchToProps = dispatch => {
   return {
-    onRemove: id => dispatch(removeTodo(id))
+    onRemove: id => dispatch(removeTodo(id)),
+    onUpdate: todo => dispatch(updateTodo(todo))
   };
 };
 
