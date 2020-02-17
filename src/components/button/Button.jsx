d4e7531730034ec380e27a01b1b98b4ac35d63f3
ptlm500 <peter_mills@hotmail.co.uk>
2020-02-17 19:20:45 +0000

    Add styles for svgs

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -1,24 +1,37 @@
 import styled from 'styled-components';
 import { theme } from '../../styles';
 
+const HEIGHT = 2;
+const HORIZONTAL_PADDING = 1;
+const VERTICAL_PADDING = 0.25;
+
 export const PrimaryButton = styled.button`
   background: ${props => props.danger ? theme.danger : theme.primary};
   border-radius: ${theme.defaultRadius};
   border: 2px solid ${props => props.danger ? theme.danger : theme.primary};
   cursor: pointer;
   color: ${theme.invertedText};
-  height: 2rem;
-  padding: 0.25rem 1rem;
+  height: ${HEIGHT}rem;
+  padding: ${VERTICAL_PADDING}rem ${HORIZONTAL_PADDING}rem;
   transition: ${theme.shortTransition};
 
-  &:hover {
+  > svg {
+    width: ${HEIGHT - 2 * VERTICAL_PADDING}rem;
+    width: ${HEIGHT - 2 * VERTICAL_PADDING}rem;
+    fill: currentColor;
+  }
+
+  fill: ${theme.invertedIcon};
+
+  &:hover:not([disabled]) {
     background: ${props => props.danger ? theme.dangerLight : theme.primaryLight};
   }
 
   &:disabled {
     border: 2px solid ${theme.disabled};
     background: ${theme.disabled};
-    color: ${theme.secondaryText};
+    color: ${theme.disabled};
+    fill: ${theme.primary};
   }
 `;
 
@@ -26,16 +39,17 @@ export const SecondaryButton = styled(PrimaryButton)`
   background: ${theme.background};
   border: 2px solid ${props => props.danger ? theme.danger : theme.primary};
   color: ${props => props.danger ? theme.danger : theme.primary};
+  fill: ${props => props.danger ? theme.danger : theme.primary};
 
-  &:hover {
+  &:hover:not([disabled])  {
     background: ${props => props.danger ? theme.danger : theme.primary};
     color: ${theme.invertedText};
+    fill: ${theme.invertedIcon};
   }
 
   &:disabled {
     background: ${theme.background};
     border: 2px solid ${theme.disabled};
-    color: ${theme.disabled};
     cursor: auto;
   }
 `;
