6c69cacdec52abb3b5fa9c62b17c0b90904f555e
ptlm500 <peter_mills@hotmail.co.uk>
2020-02-17 23:38:42 +0000

    Intl button text

--- src/components/addTodoListItem/AddTodoListItem.jsx
+++ src/components/addTodoListItem/AddTodoListItem.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import { useIntl, defineMessages, FormattedMessage } from 'react-intl';
 import { addTodo } from '../../state/actions/actions';
 import { v4 as uuid } from 'uuid';
 import AutosizingTextarea from '../autosizingTextarea';
@@ -15,6 +16,19 @@ import Input from '../input';
 import './addTodoListItem.scss';
 import { NewTodo } from '../../icons';
 
+const messages = defineMessages({
+  namePlaceholder: {
+    id: 'todo.addTodo.namePlaceholder',
+    defaultMessage: 'Enter a name',
+    description: 'Placeholder message for the add todo name field'
+  },
+  descriptionPlaceholder: {
+    id: 'todo.addTodo.descriptionPlaceholder',
+    defaultMessage: 'Enter a description',
+    description: 'Placeholder message for the add todo description field'
+  }
+});
+
 function generateTodo(name, description) {
   return {
     id: uuid(),
@@ -28,6 +42,8 @@ export function AddTodoListItem(props) {
   const {
     onAddTodo
   } = props;
+
+  const intl = useIntl();
 
   const [addingItem, setAddingItem] = useState(false);
   const [name, setName] = useState('');
@@ -50,18 +66,30 @@ export function AddTodoListItem(props) {
     ) : (
       <Card>
         <Input
-          placeholder={'Enter a name'}
+          placeholder={intl.formatMessage(messages.namePlaceholder)}
           value={name}
           onChange={e => setName(e.target.value)}
         />
         <AutosizingTextarea
-          placeholder={'Enter a description'}
+          placeholder={intl.formatMessage(messages.descriptionPlaceholder)}
           value={description}
           onChange={e => setDescription(e.target.value)}
         />
         <div className='todo-list__new-item-actions'>
-          <SecondaryButton onClick={cancel}>Cancel</SecondaryButton>
-          <PrimaryButton onClick={submitTodo}>Add</PrimaryButton>
+          <SecondaryButton onClick={cancel}>
+            <FormattedMessage
+              id="todo.addTodo.cancel"
+              description="Add todo cancel button text"
+              defaultMessage="Cancel"
+            />
+          </SecondaryButton>
+          <PrimaryButton onClick={submitTodo}>
+            <FormattedMessage
+              id="todo.addTodo.add"
+              description="Add todo add button text"
+              defaultMessage="Add"
+            />
+          </PrimaryButton>
         </div>
       </Card>
     )
