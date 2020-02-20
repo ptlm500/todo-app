40a814267411fd0704b23b788823446101e87e7f
ptlm500 <peter_mills@hotmail.co.uk>
2020-02-20 21:27:42 +0000

    Add todo api routes

--- server.js
+++ server.js
@@ -6,6 +6,21 @@ const port = process.env.PORT || 8080;
 
 acceptLanguage.languages(['en', 'de']);
 
+const todos = {
+  'test-id': {
+    id: 'test-id',
+    name: 'Test Todo',
+    description: 'Pass all tests!',
+    creationDate: '1581549548000'
+  },
+  'test-id-2': {
+    id: 'test-id-2',
+    name: 'Test Todo 2',
+    description: 'Style these things!',
+    creationDate: '1581549548000'
+  }
+};
+
 const app = express();
 
 app.use(cookieParser());
@@ -25,6 +40,48 @@ app.get('/api/locale', (req, res) => {
   console.log('/api/locale sending locale', locale);
   res.status(200)
     .json({locale});
+});
+
+app.get('/api/todos', (_, res) => {
+  console.log('/api/todos sending todos');
+  res.status(200)
+    .json({todos});
+});
+
+app.get('/api/todo/:todoId', (req, res) => {
+  const { todoId } = req.params;
+
+  if (todos[todoId]) {
+    console.log('/api/todo returning todo', todoId);
+    res.status(200)
+      .json({todo: todos[todoId]});
+  }
+  console.error(`/api/todo no todo with id ${todoId} found`);
+  res.status(404);
+});
+
+app.get('/api/todo/:todoId', (req, res) => {
+  const { todoId } = req.params;
+
+  if (todos[todoId]) {
+    console.log('/api/todo updating todo', todoId);
+    todos[todoId] = req.body.todo;
+    res.status(200)
+      .json({todo: todos[todoId]});
+  }
+});
+
+app.delete('/api/todo/:todoId', (req, res) => {
+  const { todoId } = req.params;
+
+  if (todos[todoId]) {
+    console.log('/api/todo deleted todo', todoId);
+    delete todos[todoId];
+    res.status(200)
+      .json({todoId});
+  }
+  console.error(`/api/todo no todo with id ${todoId} found`);
+  res.status(404);
 });
 
 app.get('*', (req, res) =>{
